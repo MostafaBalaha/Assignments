a92a4b5c59d9f1dc159a69f6175e37761a79b0af
MostafaBalaha <32961300+MostafaBalaha@users.noreply.github.com>
2017-11-29 05:25:28 +0200

    Add files via upload

---
+++ Assignment 4/Assignment 4/File.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_4
+{
+    interface IFile
+    {
+        void setName();
+        string getName();
+      
+        string getInfo();
+        void setExt();
+        string getExt();
+        void setSize();
+        double getSize();
+
+    }
+    class File
+    {
+        string name,info,ext;
+        double size;
+        public File()
+        {
+            name = "";
+            info = "";
+            ext = "";
+            size = 0.0;
+
+        }
+        public void setName(string x)
+        {
+            name = x;
+        }
+        public string getName()
+        {
+            return name;
+        }
+       
+        public virtual string getinfo()
+        {
+            return "This is a file :D";
+        }
+        public void setExt(string x)
+        {
+            ext = x;
+        }
+        public string getExt()
+        {
+            return ext;
+        }
+        public void setSize(double x)
+        {
+            size = x;
+        }
+        public double getSize()
+        {
+            return size;
+        }
+
+
+
+    }
+    class image :File
+    {
+        string imageQuality;
+        public image()
+        {
+            imageQuality = "Normal";
+        }
+        public override string getinfo()
+        {
+            return "This is an image, :D";
+        }
+
+        public void setQuality(string x)
+        {
+            imageQuality = x;
+        }
+
+        public string getQuality()
+        {
+           return imageQuality ;
+        }
+
+    }
+    class video :File
+    {
+        double duration;
+        public video()
+        {
+            duration = 0.0;
+        }
+
+        public override string getinfo()
+        {
+            return "This is a video, :)";
+        }
+        public void setDuration(double x)
+        {
+            duration = x;
+        }
+        public double getDuration()
+        {
+            return duration;
+        }
+    }
+}
---
+++ Assignment 4/Assignment 4/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Parent Class 
+            File myFile = new File();
+            Console.WriteLine(myFile.getinfo()); 
+            myFile.setName( "New File");
+            Console.WriteLine(myFile.getName());
+            myFile.setSize(1024);
+            Console.WriteLine(myFile.getSize());
+            myFile.setExt("txt");
+            Console.WriteLine(myFile.getExt());
+            // Child Class
+            image myImg = new image();
+            Console.WriteLine(myImg.getinfo());
+            myImg.setExt("jpeg");
+            Console.WriteLine(myImg.getExt());
+            myImg.setQuality("High");
+            Console.WriteLine(myImg.getQuality());
+            // Another Child Class
+            video myVid = new video();
+            Console.WriteLine(myVid.getinfo());
+            myVid.setExt("mp4");
+            Console.WriteLine(myVid.getExt());
+            myVid.setDuration(65);
+            Console.WriteLine(myVid.getDuration());
+
+            Console.ReadLine();
+        }
+    }
+}
